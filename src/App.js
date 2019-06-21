a403e846bb1962d723f3e9aaaa7034946470bd5e
Eric Syme <spotlightphotoseattle@gmail.com>
2019-06-21 15:43:26 -0700

    configuring onclick

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Menu from './components/homeCardComponent'
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Main from './components/MainComponent';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Eric Syme -- Portfolio</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
+        <Main />
       </div>
     );
   }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import Header from './HeaderComponent';
-
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './homeCardComponent';
+import CategoryDetail from './CategorydatailComponent';
+import { CATEGORIES } from '../shared/categories';
 
 class Main extends Component {
+
   constructor(props) {
     super(props);
+    this.state = {
+        categories: CATEGORIES,
+        selectedCategory: null
+    };
+  }
+
+  onCategorySelect(categoryId) {
+    this.setState({ selectedCategory: categoryId});
   }
 
   render() {
-
     return (
       <div>
-        <Header />
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Eric Syme -- Portfolio</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu categories={this.state.categories} onClick={(categoryId) => this.onCategorySelect(categoryId)} />
+        <CategoryDetail category={this.state.categories.filter((category) => category.id === this.state.selectedCategory)[0]} />
       </div>
     );
   }
