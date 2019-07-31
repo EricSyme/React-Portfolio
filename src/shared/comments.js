a42783474a36aaa1032b678eb3f796483bb0d4ba
Eric Syme <spotlightphotoseattle@gmail.com>
2019-07-31 12:30:05 -0700

    weather app added into projects

--- src/shared/comments.js
+++ src/shared/comments.js
@@ -42,7 +42,7 @@ export const COMMENTS =
     },
     {
         id: 5,
-        projectId: 1,
+        projectId: 2,
         rating: 5,
         comment: "This React site was built as a Single Page Application and uses a JSON server to house data that can be manipulated by the user.",
         author: "John Lemon",
@@ -50,7 +50,7 @@ export const COMMENTS =
     },
     {
         id: 6,
-        projectId: 1,
+        projectId: 2,
         rating: 4,
         comment: "Each element is a different component; Header, Footer, Menu, AboutUs, ContactUs etc. ",
         author: "Paul McVites",
@@ -58,7 +58,7 @@ export const COMMENTS =
     },
     {
         id: 7,
-        projectId: 1,
+        projectId: 2,
         rating: 3,
         comment: "Reactstrap features build the 'Cards' that hold each dish and help structure the page on the grid system.",
         author: "Michael Jaikishan",
@@ -66,7 +66,7 @@ export const COMMENTS =
     },
     {
         id: 8,
-        projectId: 1,
+        projectId: 2,
         rating: 4,
         comment: "Middleware is applied to perform error checks and data management.",
         author: "Ringo Starry",
@@ -74,7 +74,7 @@ export const COMMENTS =
     },
     {
         id: 9,
-        projectId: 1,
+        projectId: 2,
         rating: 2,
         comment: "React animations are used to provide an appealing UX.",
         author: "25 Cent",
@@ -82,42 +82,26 @@ export const COMMENTS =
     },
     {
         id: 10,
-        projectId: 2,
+        projectId: 1,
         rating: 5,
-        comment: "Imagine all the eatables, living in conFusion!",
-        author: "John Lemon",
-        date: "2012-10-16T17:57:28.556094Z"
+        comment: "The use of the openweathermap.org API enables current weather reports for an input location. ",
+        author: "const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${'APIkey'}`)\n\n",
+        date: "2019-07-28T17:57:28.556094Z"
     },
     {
         id: 11,
-        projectId: 2,
+        projectId: 1,
         rating: 4,
-        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-        author: "Paul McVites",
-        date: "2014-09-05T17:57:28.556094Z"
+        comment: "To achieve the Fahrenheit conversion from Kelvin I converted to Celsius first then to Fahrenheit.",
+        author: "const kelvin = this.props.temperature; const celsius = kelvin - 273; let fahrenheit = celsius * (9/5) + 32; fahrenheit = Math.floor(fahrenheit)",
+        date: "2019-07-28T17:57:28.556094Z"
     },
     {
         id: 12,
-        projectId: 2,
+        projectId: 1,
         rating: 3,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "2015-02-13T17:57:28.556094Z"
-    },
-    {
-        id: 13,
-        projectId: 2,
-        rating: 4,
-        comment: "Ultimate, Reaching for the stars!",
-        author: "Ringo Starry",
-        date: "2013-12-02T17:57:28.556094Z"
-    },
-    {
-        id: 14,
-        projectId: 2,
-        rating: 2,
-        comment: "It's your birthday, we're gonna party!",
-        author: "25 Cent",
-        date: "2011-12-02T17:57:28.556094Z"
+        comment: "The codeSandbox below needs to be tinkered with to use the API but the code can still be viewed through the menu button in the upper left corner.",
+        author: "Eric Syme",
+        date: "2019-07-29T17:57:28.556094Z"
     }
 ]
--- src/shared/projects.js
+++ src/shared/projects.js
@@ -10,6 +10,15 @@ export const PROJECTS =
         },
         {
             id: 1,
+            name:'API Key -- My 1st Weather App',
+            image: '/assets/images/seattle.png',
+            category: 'mains',
+            featured: false,
+            sandbox: "https://codesandbox.io/embed/myweatherapp-l5jly?fontsize=14",
+            description:'This solo project fetches an API from openweathermap.org and returns data.  This simple app uses three components and is of basic layout. The goal was to learn how to use an API key and how to structure basic data return.  One slightly challenging detail was that the temperature data from the API returns in Kelvin, and I needed to find out how to convert to Fehrenheit. The next step is to call a geo-location API and load data associated to the location of the user as well as refine the search field. '
+        },
+        {
+            id: 2,
             name:'React -- Ristorante ConFusion',
             image: '/assets/images/reactConfusion.png',
             category: 'mains',
